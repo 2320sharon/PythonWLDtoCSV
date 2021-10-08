63f38c2f984db201c97ca9a78bb3364d64ababbc
2320sharon <sharonfitzpatrick@csus.edu>
2021-10-07 17:34:55 -0700

    Program plan

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ import pandas as pd
 from skimage.io import imread
 import xml.etree.ElementTree as ET
 
-# read_wld()
+# read_wld(wld_file_path)
 # Returns a float list containing [XCellSize,YCellSize,UpperleftX,UpperleftY] 
+# @Params (wld_file_path)
+#     wld_file_path: string representing the path to the .wld file
 def read_wld(wld_file_path):
    # empty list to hold file contents as strings
    wld_array=[]
@@ -45,6 +47,12 @@ def get_coords(wld_array,rows,cols):
    yMin = (WorldY + (rows * YCellSize)) - (YCellSize / 2)
    return xMin, xMax, yMin, yMax
 
+
+#def read_xml(xml_path)
+# Returns  (["WGS 84 / UTM zone 18N"]) extracted from dataAxisToSRSAxisMapping in the XML
+# @Params (xml_path)
+#     xml_path: string representing the path to the XML file
+
 def read_xml(xml_path):
    tree = ET.parse(xml_path)
    root = tree.getroot()
@@ -68,26 +76,54 @@ def read_xml(xml_path):
    #GOAL: (["WGS 84 / UTM zone 18N"])
    return  dataAxisToSRSAxisMapping
 
+def read_jpg(jpg_path):
+   # Extract image name from path
+   jpg_name=jpg_path.split('\\')[-1];
+   print(jpg_name)
+   return jpg_name
 
-# def parse_crs():
-#    (do stuff)
-#    return crs_string
-# crs_string = parse_crs(xml_info)
+# def write_csv(xmin, xmax, ymin, ymax,crs_string)
+# Returns true on successful write
+# Write line to csv file\
+# image filename,
+# Easting min (XMin),
+# Easting max (XMax),
+# Northing min (YMin),
+# Norhting max (YMax),
+# Coordinate Reference System (e.g. wgs 84 / utm zone 18N).
+# # 
+# @Params (xml_path)
+#     xml_path: string representing the path to the XML file
 
+def write_csv(xmin, xmax, ymin, ymax,crs_string):
+  
+   write_csv(xmin, xmax, ymin, ymax,crs_string )
 
 # xml_info = read_xml(xml_file)
 wld_file_path ='PythonWLDtoCSV\sampleData\LC08_014035_20200604.wld'
 xml_path='PythonWLDtoCSV\sampleData\LC08_014035_20200604.jpg.aux.xml'
+jpg_path='PythonWLDtoCSV\sampleData\LC08_014035_20200604.jpg'
+
 wld_array=read_wld(wld_file_path)
 print(wld_array)
-rows, cols, bands = imread('PythonWLDtoCSV\sampleData\LC08_014035_20200604.jpg').shape
+rows, cols, bands = imread(jpg_path).shape
 xmin, xmax, ymin, ymax = get_coords(wld_array,rows,cols)
 print(xmin, xmax, ymin, ymax)
-read_xml(xml_path)
-
-
-
-# def write_csv():
-#    (do stuff)
+crs_string = read_xml(xml_path)
 # write_csv(xmin, xmax, ymin, ymax,crs_string )
+read_jpg(jpg_path)
+
+# Commmand line interface that takes folder containing files and gets them 
+# All in one file???
+# Check the file types and append them to respective lists
+# Once done run the program
+# Notify the user when done.
+
+# Allow user to specify the name of the csv
+
+# Allow user to specifly location to save file in 
+# Check if Linux and Windows differ
+
+
+
 
